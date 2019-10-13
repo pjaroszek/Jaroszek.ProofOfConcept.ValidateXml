572541a1d46970d76499b2ea27b0ffa75c228180
Paweł Jaroszek <pjaroszek@gmail.com>
2019-10-13 21:24:17 +0200

    add factory v3

--- ValidateXmlFileUseXsdSchemaPoC/Program.cs
+++ ValidateXmlFileUseXsdSchemaPoC/Program.cs
@@ -1,9 +1,9 @@
-﻿namespace ValidateXmlFileUseXsdSchemaPoC
+﻿using System.Text;
+
+namespace ValidateXmlFileUseXsdSchemaPoC
 {
     using System;
     using System.IO;
-    using System.Text;
-    using ValidateXmlFileUseXsdSchemaPoC.Services;
 
     internal static class Program
     {
@@ -14,18 +14,28 @@
             string[] fileXml = Directory.GetFiles(DEFAULT_DATA_DIRECTORY);
             string xsd = File.ReadAllText(@"Assets\Schemat_JPK_VAT(3)_v1-1.xsd");
 
-//            var saxon = new XmlDocumentValidateUseSaxon(xsd);
-//            var xmlValidate = new XmlDocumentValidate(xsd);
-//
-//            foreach (string file in fileXml)
-//            {
-//                var xml = File.ReadAllText(file, Encoding.UTF8);
-//                Console.WriteLine($"saxon- {file} => {saxon.Validate(xml)}");
-//                Console.WriteLine($"{file} => {xmlValidate.Validate(xml)}");
-//            }
+            //            var saxon = new XmlDocumentValidateUseSaxon(xsd);
+            //            var xmlValidate = new XmlDocumentValidate(xsd);
+            //
+            //            foreach (string file in fileXml)
+            //            {
+            //                var xml = File.ReadAllText(file, Encoding.UTF8);
+            //                Console.WriteLine($"saxon- {file} => {saxon.Validate(xml)}");
+            //                Console.WriteLine($"{file} => {xmlValidate.Validate(xml)}");
+            //            }
 
 
             var factory = new ValidateConditioner().ExecuteValidation(Actions.XmlDocument, xsd);
+            var factorySaxon = new ValidateConditioner().ExecuteValidation(Actions.Saxon, xsd);
+
+            foreach (string file in fileXml)
+            {
+                var xml = File.ReadAllText(file, Encoding.UTF8);
+
+                Console.WriteLine($"{file} => {factory.Validate(xml)}");
+                Console.WriteLine($"saxon- {file} => {factorySaxon.Validate(xml)}");
+
+            }
 
             Console.ReadKey();
         }
